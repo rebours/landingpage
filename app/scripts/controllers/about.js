66fd5e1da83b3eb0d1938b2b7a2595c7b9b78dd3
PiloOp <pierre-louis.rebours@epitech.eu>
2016-06-07 19:27:47 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name landingpageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the landingpageApp
+ */
+angular.module('landingpageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name landingpageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the landingpageApp
+ */
+angular.module('landingpageApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
